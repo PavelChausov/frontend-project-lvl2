d6078a4243f9266b75a9c9b7109c934ef1e5402b
Pavel Chausov <pavel.chausov@gmail.com>
2020-04-26 19:31:17 +0300

    второй шаг

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import Commander from 'commander';
 
 const myApp = Commander
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0');
-  // .parse(process.argv);
+  .version('0.1.0')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(`first config: ${firstConfig}`);
+    console.log(`second config: ${secondConfig}`);
+  })
+  .option('-f, --format [type]', 'output format');
 
 const runApp = () => {
   myApp.parse(process.argv);
+  if (myApp.format !== undefined) {
+    console.log(`format: ${myApp.format}`);
+  }
 };
 
 export default runApp;
