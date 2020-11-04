4fc9c22504acd6df769056b68163a2a4c166207b
Pavel Chausov <pavel.chausov@gmail.com>
2020-11-04 22:28:12 +0300

    третий шаг

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import _ from 'lodash';
+
+const app = (filepath1, filepath2) => {
+  const file1 = JSON.parse(fs.readFileSync(filepath1));
+  const file2 = JSON.parse(fs.readFileSync(filepath2));
+  const diffs = [..._.toPairs(file1), ..._.toPairs(file2)]
+    .sort(([key1], [key2]) => {
+      if (key1 > key2) {
+        return 1;
+      }
+      if (key1 < key2) {
+        return -1;
+      }
+      return 0;
+    })
+    .map(([key, value]) => {
+      if (file1[key] === value && file2[key] !== value) {
+        return [key, value, '-'];
+      }
+      if (file1[key] !== value && file2[key] === value) {
+        return [key, value, '+'];
+      }
+      return [key, value, ' '];
+    });
+  const uniq = _.uniqWith(diffs, _.isEqual);
+  const res = [
+    '{',
+    ...uniq.map(([key, value, sign]) => `  ${sign} ${key} ${value}`),
+    '}',
+  ].join('\n');
+  console.log(res);
+};
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import Commander from 'commander';
+import commander from 'commander';
+import app from './app.js';
 
-const myApp = Commander
+const myApp = commander
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
-  .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig) => {
-    console.log(`first config: ${firstConfig}`);
-    console.log(`second config: ${secondConfig}`);
-  })
+  .arguments('<filepath1> <filepath2>')
+  .action(app)
   .option('-f, --format [type]', 'output format');
 
 const runApp = () => {
